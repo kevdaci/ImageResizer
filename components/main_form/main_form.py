df44cfe49c3c3fd0cf511efdf8eff03090e6209b
kevdaci <kevdaci@gmail.com>
2023-03-18 18:09:49 -0400

    Add text input boxes

--- components/main_form/main_form.py
+++ components/main_form/main_form.py
@@ -3,17 +3,21 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, ListProperty
 from kivy.lang import Builder
 from plyer import filechooser
 from uix.upload_button import UploadButton
+from uix.dimension_text_inputs import DimensionTextInputs
 
 Builder.load_file("./components/main_form/main_form.kv")
 class MainForm(Widget):
 
 	background_color = [200/255, 230/255, 201/255, 1]
-	selection = ObjectProperty()
+	selection = ListProperty()
 	form = ObjectProperty()
+	dimension_text_inputs = ObjectProperty()
+	title_label = ObjectProperty()
+	picture_image = ObjectProperty()
 
 	def get_dimensions(self):
 		print(self.size)
@@ -23,5 +27,9 @@ class MainForm(Widget):
 
 	def store_photo_destination(self, selection):
 		self.selection = selection
-		print(self.selection)
-		print(self.form.children)
+
+	def add_dimension_text_inputs(self):
+		self.dimension_text_inputs.opacity = 1
+		print(self.dimension_text_inputs.width, self.dimension_text_inputs.height)
+		
+
---
+++ uix/dimension_text_inputs.py
@@ -0,0 +1,12 @@
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+
+Builder.load_file('./uix/dimension_text_inputs.kv')
+class DimensionTextInputs(Widget):
+
+	pass
